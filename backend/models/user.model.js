b1f7bbb0ff5ca702c0647cbd0165335593db5548
anish-9387 <anishmall3879@gmail.com>
2026-06-06 19:08:12 +0530

    feat: made the models and its schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  repositories: [{
+    default: [],
+    type: Schema.Types.ObjectId,
+    ref: 'Repository'
+  }],
+  followedUsers: [{
+    default: [],
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  starredRepositories: [{
+    default: [],
+    type: Schema.Types.ObjectId,
+    ref: 'Repository'
+  }],
+});
+
+module.exports = mongoose.model('User', userSchema);
