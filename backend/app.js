4833be75362d36580600169673b3ad1afc39ec3b
anish-9387 <anishmall3879@gmail.com>
2026-06-06 18:46:01 +0530

    feat: setup the server logic

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const { initRepo } = require("./controllers/init.controller.js");
 const { add } = require("./controllers/add.controller.js");
 const { commit } = require("./controllers/commit.controller.js");
@@ -8,7 +16,11 @@ const { push } = require("./controllers/push.controller.js");
 const { pull } = require("./controllers/pull.controller.js");
 const { revert } = require("./controllers/revert.controller.js");
 
+dotenv.config();
+
 yargs(hideBin(process.argv))
+  .command("start", "Start the server", {}, startServer)
+
   .command("init", "Initialise a new repository", {}, initRepo)
 
   .command("add <file>", "Add files to the staging area", (yargs) => {
@@ -40,7 +52,60 @@ yargs(hideBin(process.argv))
       describe: "Commit ID to revert to",
       type: "string",
     });
-  }, revert)
+  },
+  (argv) => {
+    revert(argv.commitID);
+  })
 
   .demandCommand(1, "You need to specify atleast one command")
   .help().argv;
+
+
+function startServer() {
+  const app = express();
+  const port = process.env.PORT || 3000;
+  const mongoURL = process.env.MONGODB_URL;
+
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  mongoose.connect(mongoURL).then(() => {
+    console.log("Connected to MongoDB");
+  }).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+  app.use(cors({ origin: "*" }));
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  let user = "test";
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("joinRoom", (userID) => {
+      user = userID;
+      console.log("================================");
+      console.log(`User ${user} connected`);
+      console.log("================================");
+      socket.join(user);
+    });
+  });
+
+  const db = mongoose.connection;
+  db.once("open", async() => {
+    console.log("CRUD operations called");
+  });
+
+  httpServer.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
--- backend/controllers/revert.controller.js
+++ backend/controllers/revert.controller.js
@@ -1,5 +1,29 @@
-function revert() {
-    console.log("Revert command executed");
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
+
+const readDir = promisify(fs.readdir);
+const copyFile = promisify(fs.copyFile);
+
+async function revert(commitID) {
+  const repoPath = path.resolve(process.cwd(), ".versiona");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    const commitDir = path.join(commitsPath, commitID);
+    const files = await readDir(commitDir);
+    const parentDir = path.resolve(repoPath, "..");
+
+    for (const file of files) {
+      const srcPath = path.join(commitDir, file);
+      const destPath = path.join(parentDir, file);
+      await copyFile(srcPath, destPath);
+    }
+
+    console.log(`Reverted to commit ${commitID} successfully.`);
+  } catch (err) {
+    console.error("Error reverting to commit:", err);
+  }
 }
 
 module.exports = { revert };
