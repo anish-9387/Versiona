ff296fe8c8cdfcbf92d9d9e58508c4a4cfc2507f
anish-9387 <anishmall3879@gmail.com>
2026-06-10 11:28:47 +0530

    fix: frontend and backend connectivity

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/user': '16.171.27.203:3000',
-      '/repository': '16.171.27.203:3000',
-      '/issue': '16.171.27.203:3000',
+      '/user': { target: 'http://16.171.27.203:3000', changeOrigin: true },
+      '/repository': { target: 'http://16.171.27.203:3000', changeOrigin: true },
+      '/issue': { target: 'http://16.171.27.203:3000', changeOrigin: true },
     }
   }
 })
