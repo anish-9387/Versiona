bcd65dd147c8607e9de5f437a4187216a9d40885
anish-9387 <anishmall3879@gmail.com>
2026-06-10 11:05:07 +0530

    fix: configuration of backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/user': 'http://localhost:3000',
-      '/repository': 'http://localhost:3000',
-      '/issue': 'http://localhost:3000',
+      '/user': '16.171.27.203:3000',
+      '/repository': '16.171.27.203:3000',
+      '/issue': '16.171.27.203:3000',
     }
   }
 })
